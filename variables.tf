cf10383f84d123c7f891409b1c5dfcec8bade043
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-10-22 10:27:27 +0000

    feed admin role and deploy eks

--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "env" {
 
 variable "name" {
 }
+
+variable "admin_role_arn" {
+  description = "Example: arn:aws:iam::158237111111:role/AWSReservedSSO_AdministratorAccess_17b27zzzzz385ed9"
+}
+
+variable "cluster_version" {
+  default = "1.23"
+}
