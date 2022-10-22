6d82883403df156d845fc6d25c7b9df630dc1c7a
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-10-22 10:29:26 +0000

    fill back in data

--- data.tf
+++ data.tf
@@ -4,14 +4,14 @@ data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-# data "aws_eks_cluster_auth" "this" {
-#   name = module.eks_blueprints.eks_cluster_id
-# }
-# 
-# data "aws_eks_addon_version" "latest" {
-#   for_each = toset(["kube-proxy", "vpc-cni"])
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks_blueprints.eks_cluster_id
+}
 
-#   addon_name         = each.value
-#   kubernetes_version = module.eks_blueprints.eks_cluster_version
-#   most_recent        = true
-# }
+data "aws_eks_addon_version" "latest" {
+  for_each = toset(["kube-proxy", "vpc-cni"])
+
+  addon_name         = each.value
+  kubernetes_version = module.eks_blueprints.eks_cluster_version
+  most_recent        = true
+}
