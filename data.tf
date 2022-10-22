73f6789639ab89c254761f429056000075a1f97d
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-10-22 10:00:46 +0000

    don't ref blueprint not initilized

--- data.tf
+++ data.tf
@@ -4,9 +4,9 @@ data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks_blueprints.eks_cluster_id
-}
+# data "aws_eks_cluster_auth" "this" {
+#   name = module.eks_blueprints.eks_cluster_id
+# }
 
 data "aws_eks_addon_version" "latest" {
   for_each = toset(["kube-proxy", "vpc-cni"])
